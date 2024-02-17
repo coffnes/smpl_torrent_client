2ee65a402a3b1bcc7be813a69468d6658e47805d
coffnes <coffnes@yandex.ru>
2024-02-17 22:57:27 +0300

    Add cxxopts lib for parsing options

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <cxxopts/cxxopts.hpp>
 
-int main()
+int main(int argc, const char* argv[])
 {
-    for(int i = 0; i < 100; i++) {
-        std::cout << "Hello, World!" << std::endl;
+    cxxopts::Options options("SmplTorrentClient", "A simple, multi-threaded BitTorrent client written in C++");
+
+    options.set_width(80).set_tab_expansion().add_options()
+            ("t,torrent-file", "Path to the Torrent file", cxxopts::value<std::string>())
+            ("o,output-dir", "The output directory to which the file will be downloaded", cxxopts::value<std::string>())
+            ("n,thread-num", "Number of downloading threads to use", cxxopts::value<int>()->default_value("5"))
+            ("l,logging", "Enable logging", cxxopts::value<bool>()->default_value("false"))
+            ("f,log-file", "Path to the log file", cxxopts::value<std::string>()->default_value("../logs/client.log"))
+            ("h,help", "Print arguments and their descriptions");
+
+    try
+    {
+        auto parsedOptions = options.parse(argc, argv);
+        if(parsedOptions.count("help"))
+        {
+            std::cout << options.help() << std::endl;
+            return 0;
+        }
     }
+    catch(std::exception& e)
+    {
+        std::cout << "Error while parsing options: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
 }
